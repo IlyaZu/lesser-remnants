ac6dbb6e3ede006615db92f73b19c38f9cf24416
IlyaZu <ilya.zush@gmail.com>
2023-11-10 17:01:32 +0000

    Add notice of modification to source
    This is done to be in compliance with GPL 3.0 section 5. Conveying
    Modified Source Versions.

--- src/rotp/model/colony/Colony.java
+++ src/rotp/model/colony/Colony.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015-2020 Ray Fowler
+ * Modifications Copyright 2023 Ilya Zushinskiy
  * 
  * Licensed under the GNU GeneraFl Public License, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
