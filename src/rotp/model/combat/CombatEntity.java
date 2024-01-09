e8274ff215e93192fcd3f1956afae5d71e68bfdd
IlyaZu <ilya.zush@gmail.com>
2024-01-09 18:00:38 +0000

    Clean up code

--- src/rotp/model/combat/CombatEntity.java
+++ src/rotp/model/combat/CombatEntity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015-2020 Ray Fowler
- * Modifications Copyright 2023 Ilya Zushinskiy
+ * Modifications Copyright 2023-2024 Ilya Zushinskiy
  * 
  * Licensed under the GNU General Public License, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
@@ -71,7 +71,6 @@ public abstract class CombatEntity implements Base {
     public boolean cloaked = false;
     public boolean canCloak = false;
     public boolean canTeleport = false;
-    public boolean atLastColony = false;
     public float damageSustained = 0;
     public boolean attacked = false;
     public CombatEntity target;
