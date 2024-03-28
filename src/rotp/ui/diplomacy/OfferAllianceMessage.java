905ffc0bd17e47c889bbf4c3b6951a7e2207d742
IlyaZu <ilya.zush@gmail.com>
2024-03-28 23:15:51 +0000

    Strip trailing whitespaces

--- src/rotp/ui/diplomacy/OfferAllianceMessage.java
+++ src/rotp/ui/diplomacy/OfferAllianceMessage.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015-2020 Ray Fowler
+ * Modifications Copyright 2024 Ilya Zushinskiy
  * 
  * Licensed under the GNU General Public License, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,23 +31,23 @@ public class OfferAllianceMessage extends TurnNotificationMessage {
     @Override
     public boolean enabled(int i)       { return true; }
     @Override
-    public String reply(int i)          { 
+    public String reply(int i)          {
         switch (i) {
             case 0 : return text("DIPLOMACY_ACCEPT_FORM_ALLIANCE");
             case 1 : return text("DIPLOMACY_DECLINE_OFFER");
         }
-        return ""; 
+        return "";
     }
     @Override
     public void select(int i) {
         log("OfferAllianceMessage - selected: ", str(i));
         switch(i) {
-        case 0: 
+        case 0:
             DiplomaticReply reply = player().diplomatAI().acceptOfferAlliance(diplomat());
             reply.resumeTurn(true);
-            DiplomaticMessage.reply(DiplomacyRequestReply.create(diplomat(), reply));	
+            DiplomaticMessage.reply(DiplomacyRequestReply.create(diplomat(), reply));
             break;
-        case 1: 
+        case 1:
         default:
             escape(); break;
         }
@@ -57,8 +58,8 @@ public class OfferAllianceMessage extends TurnNotificationMessage {
         session().resumeNextTurnProcessing();
     }
     @Override
-    public String decode(String encodedMessage) { 
-        String s1 = super.decode(encodedMessage); 
+    public String decode(String encodedMessage) {
+        String s1 = super.decode(encodedMessage);
         return s1;
     }
 }
