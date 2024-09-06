8207287d85a6abb44a780afcd895f4d90872f4c6
IlyaZu <ilya.zush@gmail.com>
2024-09-06 13:21:47 +0100

    Create Drift Incident
    This is done to keep relationship changes happening through a single
    stream and enable some future refactor work I have planned.

---
+++ src/rotp/model/incidents/DriftRelationsIncident.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Ilya Zushinskiy
+ * 
+ * Licensed under the GNU General Public License, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package rotp.model.incidents;
+
+import rotp.model.empires.DiplomaticEmbassy;
+import rotp.model.empires.EmpireView;
+
+public class DriftRelationsIncident extends DiplomaticIncident {
+    private static final long serialVersionUID = 1L;
+
+    public static void create(EmpireView view) {
+        DiplomaticEmbassy embassy = view.embassy();
+        float severity = (embassy.baseRelations() - embassy.relations()) / 50;
+        embassy.addIncident(new DriftRelationsIncident(severity));
+    }
+    
+    private DriftRelationsIncident(float severity) {
+        super(severity);
+    }
+    
+    @Override
+    public boolean isForgotten(){
+        // Cannot be displayed as there is no text for this incident.
+        return true;
+    }
+
+    @Override
+    public String title() {
+        // Due to licensing restrictions there is currently no text for this incident.
+        return "";
+    }
+
+    @Override
+    public String description() {
+        // Due to licensing restrictions there is currently no text for this incident.
+        return "";
+    }
+    
+}
