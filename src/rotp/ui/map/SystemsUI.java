9ba4dd12c22df2c66772820655d2ebf928f07283
IlyaZu <ilya.zush@gmail.com>
2025-03-15 15:52:51 +0000

    Correct string value
    troopMsg would be used to concatenate with other strings to form user
    facing messages. This resulted in "null" being printed to the user.
    This is almost certainly a bug and has been replaced with empty string
    to simply omit the troop message when there are no troops to message
    about.

--- src/rotp/ui/map/SystemsUI.java
+++ src/rotp/ui/map/SystemsUI.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015-2020 Ray Fowler
- * Modifications Copyright 2023-2024 Ilya Zushinskiy
+ * Modifications Copyright 2023-2025 Ilya Zushinskiy
  * 
  * Licensed under the GNU General Public License, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
@@ -693,7 +693,7 @@ public final class SystemsUI extends BasePanel implements IMapHandler, ActionLis
         int num = pl.transportsInTransit(sv.system());
         String troopMsg;
         if (num == 0)
-            troopMsg = null;
+            troopMsg = "";
         else if (sysEmp == pl)
             troopMsg = " "+text("SYSTEMS_EXPLOIT_TRANSPORTS",str(num));
         else {
