f4df333e713242e40c51b5d73fd9b6d0452206b2
IlyaZu <ilya.zush@gmail.com>
2025-03-22 14:36:48 +0000

    Remove redundant code
    A lot of these override the exact same default implementation from the
    interface or aren't used or are implied if not present. Remove
    redundant code to reduce information noise.

--- src/rotp/ui/sprites/MapSprite.java
+++ src/rotp/ui/sprites/MapSprite.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015-2020 Ray Fowler
+ * Modifications Copyright 2025 Ilya Zushinskiy
  * 
  * Licensed under the GNU General Public License, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +16,8 @@
  */
 package rotp.ui.sprites;
 
-import java.awt.Graphics2D;
 import rotp.model.Sprite;
 import rotp.model.galaxy.IMappedObject;
-import rotp.model.galaxy.StarSystem;
-import rotp.ui.main.GalaxyMapPanel;
-import rotp.ui.map.IMapHandler;
 import rotp.util.Base;
 
 public abstract class MapSprite implements Base, Sprite {
@@ -32,32 +29,6 @@ public abstract class MapSprite implements Base, Sprite {
     @Override
     public void hovering(boolean b)             { hovering = b; }
     @Override
-    public boolean persistOnClick()             { return false; }
-    @Override
     public IMappedObject source()               { return source; }
     public void source(IMappedObject o)         { source = o; }
-    @Override
-    public void repaint(GalaxyMapPanel map)     { map.repaint(); }
-    @Override
-    public int mapX(GalaxyMapPanel map)         { return map.mapX(source().x()); }
-    @Override
-    public int mapY(GalaxyMapPanel map)         { return map.mapY(source().y()); }
-    @Override
-    public int centerMapX(GalaxyMapPanel map)   { return mapX(map); }
-    @Override
-    public int centerMapY(GalaxyMapPanel map)   { return mapY(map); }
-    public void use(MapSprite s, IMapHandler ui)        { }
-
-    public MapSprite() { }
-
-    @Override
-    public StarSystem starSystem()   { return null; }
-    @Override
-    public abstract boolean isSelectableAt(GalaxyMapPanel map, int mapX, int mapY);
-    @Override
-    public abstract void draw(GalaxyMapPanel map, Graphics2D g2);
-    @Override
-    public void click(GalaxyMapPanel map, int count, boolean rightClick, boolean sound)        { }
-    @Override
-    public void wheel(GalaxyMapPanel map, int count, boolean sound)        { }
 }
