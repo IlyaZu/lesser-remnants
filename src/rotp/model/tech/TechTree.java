d3304ce123ceb2269c8d4c4ddbbff3f8d5055d36
IlyaZu <ilya.zush@gmail.com>
2026-01-08 18:16:31 +0000

    Correct avgTechLevel() type
    avgTechLevel() can never return null.

--- src/rotp/model/tech/TechTree.java
+++ src/rotp/model/tech/TechTree.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015-2020 Ray Fowler
- * Modifications Copyright 2023-2025 Ilya Zushinskiy
+ * Modifications Copyright 2023-2026 Ilya Zushinskiy
  * 
  * Licensed under the GNU General Public License, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
@@ -463,7 +463,7 @@ public final class TechTree implements Base, Serializable {
             lvl = Math.max(lvl,cat.techLevel());
         return lvl;
     }
-    public Float avgTechLevel() {
+    public float avgTechLevel() {
         float sum = 0;
         for (TechCategory cat: category)
             sum += cat.techLevel();
