4a4adbfce0d85c6f55414fc4d737efa221cf1040
IlyaZu <ilya.zush@gmail.com>
2026-04-12 00:41:03 +0100

    Remove needless indirection
    As well as a needless constructor parameter.

--- src/rotp/model/empires/SpyNetwork.java
+++ src/rotp/model/empires/SpyNetwork.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015-2020 Ray Fowler
- * Modifications Copyright 2023-2025 Ilya Zushinskiy
+ * Modifications Copyright 2023-2026 Ilya Zushinskiy
  * 
  * Licensed under the GNU General Public License, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
@@ -468,12 +468,11 @@ public final class SpyNetwork implements Base, Serializable {
         float cost = costForNextSpy();
 
         while (allocationBC >= cost) {
-            addNewSpy();
+            activeSpies.add(new Spy());
             allocationBC -= cost;
             cost = costForNextSpy();
         }
     }
-    private void addNewSpy() { activeSpies.add(new Spy(this));  }
 
     private float costForNextSpy() {
         float spyCost = owner().baseSpyCost();
